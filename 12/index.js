418643492b7c1946dbbe3833031fd8c776a73c11
Edfram <edfram.guerra@gmail.com>
2022-09-06 19:59:08 -0300

    Resolucao do exercicio 12 letra B

--- 12/index.js
+++ 12/index.js
@@ -1,4 +1,4 @@
-const diaDaSemana = 3;
+const diaDaSemana = 8;
 
 if (diaDaSemana === 1) {
     console.log("segunda");
@@ -14,4 +14,6 @@ if (diaDaSemana === 1) {
     console.log("sabado");
 } else if (diaDaSemana === 7) {
     console.log("domingo");
-} 
+} else if (diaDaSemana < 1 || diaDaSemana > 7) {
+    console.log("O dia da semana informado não é válido");
+}
