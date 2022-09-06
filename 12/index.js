5e41f35245ce8a55e4e5ac67be730acae6fc1268
Edfram <edfram.guerra@gmail.com>
2022-09-06 19:50:14 -0300

    Resolucao do exercicio 12 letra A

--- 12/index.js
+++ 12/index.js
@@ -1,1 +1,17 @@
 const diaDaSemana = 3;
+
+if (diaDaSemana === 1) {
+    console.log("segunda");
+} else if (diaDaSemana === 2) {
+    console.log("terça");
+} else if (diaDaSemana === 3) {
+    console.log("quarta");
+} else if (diaDaSemana === 4) {
+    console.log("quinta");
+} else if (diaDaSemana === 5) {
+    console.log("sexta");
+} else if (diaDaSemana === 6) {
+    console.log("sabado");
+} else if (diaDaSemana === 7) {
+    console.log("domingo");
+} 
