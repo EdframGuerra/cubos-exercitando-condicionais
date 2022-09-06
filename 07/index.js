22d8ab076d4e4133b1c3e49c0a258b14280611b9
Edfram <edfram.guerra@gmail.com>
2022-09-06 12:43:56 -0300

    Resolucao do exercicio 07

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,13 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 3_000_000;
+const limiteParaPagamentoDeIpostoEmCentavos = 2855970 * 100;
 
-//seu código aqui
+
+if (aposentada || portadoraDeDoenca) {
+    console.log("ISENTA");
+} else if (totalDeRendimentos * 100 <= limiteParaPagamentoDeIpostoEmCentavos) {
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+} else {
+    console.log("PEGA LEAO");
+}
