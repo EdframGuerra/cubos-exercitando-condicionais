b0d98bb764dfe7d781fe2e0bf46b0e84bc5eeb3f
Edfram <edfram.guerra@gmail.com>
2022-09-06 22:44:22 -0300

    Resolucao do exercicio 16

--- 16/index.js
+++ 16/index.js
@@ -1,5 +1,10 @@
-//Só é considerado de maior se a idade for maior ou igual a 18.
 const idadeDoAluno = 16;
-
-//valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+const ehEmancipado = (idadeDoAluno >= 18 || possuiResponsavel);
+
+if (ehEmancipado) {
+    console.log("Rematrícula realizada com sucesso");
+
+} else {
+    console.log("Não é possível fazer a rematrícula");
+}
