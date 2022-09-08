15d431c2a55ce61a3bc7888dc8a5bb1f8b289c58
Edfram <edfram.guerra@gmail.com>
2022-09-08 19:27:19 -0300

    Resolucao do exercicio 10

--- 10/index.js
+++ 10/index.js
@@ -8,10 +8,13 @@ const numeros = "1 2 3 4 5 6 7 8 9 0";
 
 if (caractere === "A" || caractere === "E" || caractere === "I" || caractere === "O" || caractere === "U") {
     console.log("Vogal Maiuscula");
+
 } else if (caractere === "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u") {
     console.log("Vogal Minucula");
+
 } else if (caractere === "1" || caractere === "2" || caractere === "3" || caractere === "4" || caractere === "5" || caractere === "6" || caractere === "7" || caractere === "8" || caractere === "9" || caractere === "0") {
     console.log("numeral");
+
 } else {
     console.log("Consoante")
 }
