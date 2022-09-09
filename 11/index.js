42cc38f29920f79aa9ff9371bb685545d14f374e
Edfram <edfram.guerra@gmail.com>
2022-09-08 22:00:25 -0300

    Resolucao do exercicio 11 letra B

--- 11/index.js
+++ 11/index.js
@@ -4,8 +4,17 @@ const mesesDecorridos = 12;
 const totalJaPagoPeloAluno = 1_000_000;
 const valorDaParcela = (rendaMensalEmCentavos * 18 / 100);
 
-if (rendaMensalEmCentavos > 200_000 && mesesDecorridos < 60 || totalJaPagoPeloAluno > 1_799_999) {
-    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
+//if (rendaMensalEmCentavos > 200_000 && mesesDecorridos < 60 || totalJaPagoPeloAluno > 1_799_999) {
+//  console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
+//} else {
+// console.log("Sem valor a pagar");
+//}
+
+if (rendaMensalEmCentavos <= 200_000) {
+    console.log("O aluno não poassui renda minima para pagar parcela");
+} else if (mesesDecorridos > 59 || totalJaPagoPeloAluno > 1_799_999) {
+    console.log("O aluno já quitou a divida");
 } else {
-    console.log("Sem valor a pagar");
+    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
 }
+
