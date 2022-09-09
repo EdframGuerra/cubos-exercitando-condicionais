f026018aca6eab7319174c110f9bd7ba8020bde6
Edfram <edfram.guerra@gmail.com>
2022-09-08 21:58:32 -0300

    Resolucao do exercicio 11 letra A

--- 11/index.js
+++ 11/index.js
@@ -2,11 +2,10 @@
 const rendaMensalEmCentavos = 300_000;
 const mesesDecorridos = 12;
 const totalJaPagoPeloAluno = 1_000_000;
+const valorDaParcela = (rendaMensalEmCentavos * 18 / 100);
 
-
-if (rendaMensalEmCentavos > 200000 && mesesDecorridos < 60 && totalJaPagoPeloAluno <= 1800000) {
-    const valorDaParcela = (rendaMensalEmCentavos * 18 / 100) / 100;
-    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela}`);
+if (rendaMensalEmCentavos > 200_000 && mesesDecorridos < 60 || totalJaPagoPeloAluno > 1_799_999) {
+    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
 } else {
-    console.log("Esse mês o aluno não pagara a parcela");
+    console.log("Sem valor a pagar");
 }
