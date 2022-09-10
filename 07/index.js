e596cc11c5fa34321dcb6b7659041e2edc6ba15a
Edfram <edfram.guerra@gmail.com>
2022-09-10 11:56:15 -0300

    Exercicio resolvido e revisado

--- 07/index.js
+++ 07/index.js
@@ -1,12 +1,13 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3_000_000;
-const limiteParaPagamentoDeIpostoEmCentavos = 2855970 * 100;
+const tetoLimiteParaPagamentoDoImposto = 2855970
+const limiteParaPagamentoDeIpostoEmCentavos = tetoLimiteParaPagamentoDoImposto * 100;
 
 
 if (aposentada || portadoraDeDoenca) {
     console.log("ISENTA");
-} else if (totalDeRendimentos * 100 <= limiteParaPagamentoDeIpostoEmCentavos) {
+} else if (totalDeRendimentos <= limiteParaPagamentoDeIpostoEmCentavos) {
     console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
 } else {
     console.log("PEGA LEAO");
