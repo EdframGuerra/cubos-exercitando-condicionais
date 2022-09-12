6ab58eaf33481d4e01d4d63acdf55294191d9d63
Edfram <edfram.guerra@gmail.com>
2022-09-11 21:21:11 -0300

    Exercicio resolvido e revisado

--- 13/index.js
+++ 13/index.js
@@ -3,16 +3,13 @@ const valorDoProduto = 13000;
 
 if (tipoDePagamento === "credito") {
     const valorFinal = valorDoProduto - (valorDoProduto * 5 / 100);
-
     console.log(`o valor final é R$ ${(valorFinal / 100).toFixed(2)}`);
 
 } else if (tipoDePagamento === "cheque") {
     const valorFinal = valorDoProduto - (valorDoProduto * 3 / 100);
-
     console.log(`o valor final é R$ ${(valorFinal / 100).toFixed(2)}`);
 
 } else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
     const valorFinal = valorDoProduto - (valorDoProduto * 10 / 100);
-
     console.log(`o valor final é R$ ${(valorFinal / 100).toFixed(2)}`);
 }
