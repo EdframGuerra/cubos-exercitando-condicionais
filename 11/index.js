3986eebf1b8c2dc62ef1bb9379b4c87366d5316c
Edfram <edfram.guerra@gmail.com>
2022-09-11 21:10:58 -0300

    Exercicio resolvido e revisado

--- 11/index.js
+++ 11/index.js
@@ -2,19 +2,14 @@
 const rendaMensalEmCentavos = 300_000;
 const mesesDecorridos = 12;
 const totalJaPagoPeloAluno = 1_000_000;
-const valorDaParcela = (rendaMensalEmCentavos * 18 / 100);
-
-//if (rendaMensalEmCentavos > 200_000 && mesesDecorridos < 60 || totalJaPagoPeloAluno > 1_799_999) {
-//  console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
-//} else {
-// console.log("Sem valor a pagar");
-//}
+const valorDaParcela = ((rendaMensalEmCentavos / 100) * 18 / 100);
 
 if (rendaMensalEmCentavos <= 200_000) {
     console.log("O aluno não poassui renda minima para pagar parcela");
+
 } else if (mesesDecorridos > 59 || totalJaPagoPeloAluno > 1_799_999) {
     console.log("O aluno já quitou a divida");
 } else {
-    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela / 100}`);
+    console.log(`O valor a pagar pelo aluno é de R$ ${valorDaParcela}`);
 }
 
