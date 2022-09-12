f6f6b8152cd65c419118d8aa6ae05fd2c76aa5a2
Edfram <edfram.guerra@gmail.com>
2022-09-11 21:36:30 -0300

    Exercicio resolvido e revisado

--- 17/index.js
+++ 17/index.js
@@ -1,11 +1,8 @@
 const valorDoProduto = 100_000;
 const quantidadeDoParcelamento = 10;
 const valorPago = 300;
+const saldoDevedor = (valorDoProduto / 100) - valorPago;
+const valorDasParcelas = (valorDoProduto / 100) / quantidadeDoParcelamento;
+const parcelasAPagar = saldoDevedor / valorDasParcelas;
 
-if (valorDoProduto && valorPago) {
-    const saldoDevedor = (valorDoProduto / 100) - valorPago;
-    const valorDasParcelas = (valorDoProduto / 100) / quantidadeDoParcelamento;
-    const parcelasAPagar = saldoDevedor / valorDasParcelas;
-
-    console.log(`Faltam ${parcelasAPagar} parcelas que corresponde R$ ${saldoDevedor}`);
-}
+console.log(`Faltam ${parcelasAPagar} parcelas que corresponde R$ ${saldoDevedor}`);
